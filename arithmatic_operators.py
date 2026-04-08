a44ed802416cdc03e848e8061102ceeed03dc44d
Nithya <nithyadevdas2003@gmail.com>
2026-04-08 12:18:55 +0530

    Arithmetic operators

---
+++ arithmatic_operators.py
@@ -0,0 +1,70 @@
+# x = 7
+# y = 2
+# # normal division
+# print(x/y) 
+# # floor division  
+# print(x//y)
+
+# Average of 3 Numbers
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# c = int(input("Enter c: "))
+# print((a+b+c)/2)
+
+# Convert Minutes to Hours
+# x = int(input("Enter no.of minutes: "))
+# print(x//60)
+
+# Simple Interest
+# principal = int(input("Enter the principal: "))
+# rate = int(input("Enter rate: "))
+# time = int(input("Enter time: "))
+# si = ((principal*rate*time)/100)
+# print(si)
+
+# Split Bill Among Friends
+# Total amount + number of people → find how much each person pays.
+# amount = int(input("Enter amount: "))
+# people = int(input("Enter the no.of people: "))
+# xoxo = (amount/people)
+# print(xoxo)
+
+# Area of Rectangle
+# Take length and breadth → calculate area.
+# l = int(input("Enter the length: "))
+# b = int(input("Enter the breadth: "))
+# area = l*b
+# print("Area of the rectanbgle is: ", area)
+
+# Area of Circle
+# radius = int(input("Enter the radius of a circle: "))
+# area = radius *radius * 3.14
+# print("Area of the circle is : ", area)
+
+# find the last digit usiing % operator
+# num = int(input("Enter the number: "))
+# print(num % 10)
+
+# Sum of Digits (2-digit number)
+# Example: 45 → 4 + 5
+# a = input("Enter a: ")
+# b = input("Enter b: ")
+# sum_of_digits = a + b
+# print(sum_of_digits)
+
+# Swap Two Numbers (without third variable)
+# Use arithmetic operators only.
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# a = a + b
+# b = a - b
+# a = a - b
+# print("a:",a)
+# print("b:",b)
+
+# Check Even or Odd
+# n = int(input("Enter a number: "))
+# if n % 2 == 0:
+#     print("It is an even number")
+# else:
+#     print("It is an odd number")
