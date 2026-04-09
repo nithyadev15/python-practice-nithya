9e5396e97ef29beca5e9a31b72d8e8a58fac2ee9
Nithya <nithyadevdas2003@gmail.com>
2026-04-09 20:47:39 +0530

    comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,28 @@
+# It is used to compare two values
+def test_Comparison_operators():
+    # Equal
+    number = 5
+    assert number == 5
+
+    # Not Equal
+    number = 5
+    assert number != 3
+
+    # Greater than
+    number = 5
+    assert number > 3
+
+    # Lower than
+    number = 5
+    assert number < 8
+
+    # Greater than or equal to
+    number = 5
+    assert number >= 5
+    assert number >= 4
+
+    # Lesser than or equal to
+    number = 5
+    assert number <= 4
+    assert number <= 5
+
