f153bdf6a9561571378dc285fc29bb8d23fa030f
Nithya <nithyadevdas2003@gmail.com>
2026-04-09 15:58:40 +0530

    bitwise operators

---
+++ bitwise_operators.py
@@ -0,0 +1,27 @@
+# a = 5
+# b = 3
+# result = a & b
+# print(result)
+
+# a = 5
+# b = 3
+# result = a | b
+# print(result)
+
+# a = 5
+# b = 3
+# result = a ^ b
+# print(result)
+
+# a = 5
+# result = ~a
+# print(result)
+
+# swap two numbers using XOR
+a = 5
+b = 3
+a= a ^ b
+b = a ^ b
+a = a ^ b
+print(a, b)
+
